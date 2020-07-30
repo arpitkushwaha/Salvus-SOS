5f7424279d617f716db0e5421d2676af12cc6e6f
Arpit Kushwaha <arpitkushwah01@gmail.com>
2020-07-31 05:05:59 +0530

    3 Dot Menu Added

--- app/src/main/java/com/dev/salvus/activities/HomeActivity.java
+++ app/src/main/java/com/dev/salvus/activities/HomeActivity.java
@@ -4,20 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.ActionBar;
-import android.app.AlertDialog;
+import android.app.ActivityManager;
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.dev.salvus.MainActivity;
 import com.dev.salvus.R;
 import com.dev.salvus.databinding.ActivityHomeBinding;
+import com.dev.salvus.util.MyService;
 import com.dev.salvus.util.Preferences;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
@@ -26,6 +29,7 @@ public class HomeActivity extends AppCompatActivity {
 
     ActivityHomeBinding binding;
     Preferences preferences;
+    private static final String TAG = "HomeActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,8 @@ public class HomeActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
+
+        service();
 
         binding.policecar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,17 +64,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        binding.logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                preferences.setPhone("");
-                preferences.setPassword("");
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-                finish();
-
-            }
-        });
 
         binding.feelingUnsafe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +73,8 @@ public class HomeActivity extends AppCompatActivity {
                 dialog.setContentView(R.layout.dialog_report_this_place);
                 Window window = dialog.getWindow();
                 window.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
-                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                //dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.translucent_black);
 
                 dialog.setTitle("Report this place");
 
@@ -99,5 +95,84 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
+        binding.more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Dialog dialog = new Dialog(HomeActivity.this);
+                dialog.setContentView(R.layout.dialog_more);
+                Window window = dialog.getWindow();
+                window.setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+                //dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.translucent_black);
+
+                dialog.setTitle("More");
+
+                dialog.findViewById(R.id.morecancel_button).setOnClickListener(v1 -> {
+                    dialog.cancel();
+                });
+
+
+                Button service = dialog.findViewById(R.id.service_button);
+                service.setOnClickListener(v1 -> {
+                    if (isMyServiceRunning(MyService.class)) {
+                    Snackbar.make(dialog.findViewById(R.id.parent_moreview),"Service Stopped", BaseTransientBottomBar.LENGTH_LONG).show();
+                    stopService(new Intent(HomeActivity.this, MyService.class));
+                    service.setText("Start Service");
+                } else {
+                    Snackbar.make(dialog.findViewById(R.id.parent_moreview),"Service Started", BaseTransientBottomBar.LENGTH_LONG).show();
+                    startService(new Intent(HomeActivity.this, MyService.class));
+                    service.setText("Stop Service");
+                }
+                });
+
+
+                dialog.findViewById(R.id.logout_button).setOnClickListener(v1 -> {
+                    preferences.setPhone("");
+                    preferences.setPassword("");
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                    finish();
+                });
+
+                dialog.show();
+            }
+        });
+
+
     }
+
+    private void service()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1)
+        {
+            setShowWhenLocked(true);
+            setTurnScreenOn(true);
+            KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+            if(keyguardManager!=null)
+                keyguardManager.requestDismissKeyguard(this, null);
+        }
+        else
+        {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        }
+        startService(new Intent(getApplicationContext(), MyService.class));
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        try {
+            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                if (serviceClass.getName().equals(service.service.getClassName())) {
+                    return true;
+                }
+            }
+        }
+        catch(Exception e){
+            Log.e(TAG,"isMyServiceRunning() Exception : "+e);
+        }
+        return false;
+    }
+
 }
---
+++ app/src/main/java/com/dev/salvus/util/MyReceiver.java
@@ -0,0 +1,58 @@
+package com.dev.salvus.util;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.dev.salvus.activities.HomeActivity;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    public static boolean wasScreenOn = true;
+    private int count=0;
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        Log.e("LOB","onReceive");
+        //final Handler handler = new Handler();
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+
+            // do whatever you need to do here
+            //Toast.makeText(context, "SCREENOFF = "+count, Toast.LENGTH_SHORT).show();
+            wasScreenOn = false;
+            ++count;
+            Log.e("LOB","wasScreenOn"+wasScreenOn);
+
+//        } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+//
+//            // and do whatever you need to do here
+//            Toast.makeText(context, "SCREENON", Toast.LENGTH_SHORT).show();
+//            wasScreenOn = true;
+//            ++count;
+
+        } else if(count==4){
+
+            Log.e("LOB", "userpresent");
+            Log.e("LOB", "wasScreenOn" + wasScreenOn);
+            Intent i = new Intent(context, HomeActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+            count=0;
+        }
+        else if(count>4)
+        {
+            count=0;
+        }
+
+        final Runnable r = new Runnable() {
+            public void run() {
+                //
+                //Toast.makeText(context, "New Thread = "+count, Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+        //handler.postDelayed(r, 5000);
+
+    }
+}
